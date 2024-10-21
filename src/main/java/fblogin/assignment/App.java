42781e8c6841fb19ac645dd65007fb3db2b8b22c
andreareyes <dreyarey@gmail.com>
2024-10-21 17:22:52 -0400

    changes 1234

--- src/main/java/fblogin/assignment/App.java
+++ src/main/java/fblogin/assignment/App.java
@@ -1,9 +1,13 @@
 package fblogin.assignment;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
